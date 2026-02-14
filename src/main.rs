a9783beb8216b05afe6b0d3898302b3a2f9206db
Enzo Manzinali <enzomanzinali@protonmail.com>
2026-02-14 23:31:18 +0100

    Rot word implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+fn rot_word(word: [u8; 4]) -> [u8; 4] {
+    [word[1], word[2], word[3], word[0]]
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn rot_word_test() {
+        let word = [0x00, 0x01, 0x02, 0x03];
+        let expected = [0x01, 0x02, 0x03, 0x00];
+        assert_eq!(rot_word(word), expected);
+    }
 }
