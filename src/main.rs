6a8b92d9c9d885f0a9251a175979093be2168085
Enzo Manzinali <enzomanzinali@protonmail.com>
2026-02-15 13:46:05 +0100

    Encryption working

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 mod config;
 mod tests;
 
+use hex;
+
 fn main() {
-    let state: [u8; 16] = "this is one text"
+    let plaintext: [u8; 16] = "theblockbreakers"
         .as_bytes()
         .try_into()
         .expect("state is not 16 bytes !");
-    print_state(state);
+    let key: [u8; 16] = hex::decode("2b7e151628aed2a6abf7158809cf4f3c")
+        .unwrap()
+        .try_into()
+        .unwrap();
+    print_state(encrypt(plaintext, key));
 }
 
 fn rot_word(word: [u8; 4]) -> [u8; 4] {
@@ -74,7 +80,7 @@ fn print_state(state: [u8; 16]) {
 fn sub_bytes(state: [u8; 16]) -> [u8; 16] {
     let mut modified = [0u8; 16];
     for i in 0..16 {
-        modified[i as usize] = config::SBOX[i as usize];
+        modified[i as usize] = config::SBOX[state[i] as usize];
     }
     modified
 }
@@ -109,4 +115,25 @@ fn mix_columns(state: [u8; 16]) -> [u8; 16] {
 
 fn add_round_key(state: [u8; 16], round_key: [u8; 16]) -> [u8; 16] {
     std::array::from_fn(|i| state[i] ^ round_key[i])
+}
+
+fn encrypt(plaintext: [u8; 16], key: [u8; 16]) -> [u8; 16] {
+    let keys = key_expansion(key);
+    // pre-whitening
+    let mut encrypted = add_round_key(plaintext, keys[0]);
+
+    // round 1 to 10
+    for round in 1..10 {
+        encrypted = sub_bytes(encrypted);
+        encrypted = shift_rows(encrypted);
+        encrypted = mix_columns(encrypted);
+        encrypted = add_round_key(encrypted, keys[round]);
+    }
+
+    // last round (no mixing)
+    encrypted = sub_bytes(encrypted);
+    encrypted = shift_rows(encrypted);
+    encrypted = add_round_key(encrypted, keys[10]);
+
+    encrypted
 }
