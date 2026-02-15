d831bed95fd4ef777c3f808742bc5b274d980a1c
Enzo Manzinali <enzomanzinali@protonmail.com>
2026-02-15 14:59:26 +0100

    more tests i guess

--- src/aes.rs
+++ src/aes.rs
@@ -348,4 +348,53 @@ mod tests {
         calculated = add_round_key(calculated, key);
         assert_eq!(calculated, expected);
     }
+
+    #[test]
+    fn test_decrypt() {
+        let key: [u8; 16] = [
+            0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x09, 0xcf, 0x4f, 0x3c,
+            0x76, 0x2e,
+        ];
+        let plaintext: [u8; 16] = [
+            0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37,
+            0x07, 0x34,
+        ];
+        let encrypted: [u8; 16] = encrypt(plaintext, key);
+
+        let decrypted = decrypt(encrypted, key);
+        assert_eq!(decrypted, plaintext,);
+    }
+
+    #[test]
+    fn test_shift_rows_inverse() {
+        let state: [u8; 16] = [
+            0x00, 0x01, 0x02, 0x03, 0x10, 0x11, 0x12, 0x13, 0x20, 0x21, 0x22, 0x23, 0x30, 0x31,
+            0x32, 0x33,
+        ];
+        let shifted = shift_rows(state);
+        let unshifted = shift_rows_inverse(shifted);
+        assert_eq!(unshifted, state);
+    }
+
+    #[test]
+    fn test_sub_bytes_inverse() {
+        let state: [u8; 16] = [
+            0x32, 0x88, 0x31, 0xe0, 0x43, 0x5a, 0x31, 0x37, 0xf6, 0x30, 0x98, 0x07, 0xa8, 0x8d,
+            0xa2, 0x34,
+        ];
+        let substituted = sub_bytes(state);
+        let unsubstituted = sub_bytes_inverse(substituted);
+        assert_eq!(unsubstituted, state);
+    }
+
+    #[test]
+    fn test_mix_columns_inverse() {
+        let state: [u8; 16] = [
+            0xdb, 0x13, 0x53, 0x45, 0xf2, 0x0a, 0x22, 0x5c, 0x01, 0x01, 0x01, 0x01, 0xc6, 0xc6,
+            0xc6, 0xc6,
+        ];
+        let mixed = mix_columns(state);
+        let unmixed = mix_columns_inverse(mixed);
+        assert_eq!(unmixed, state);
+    }
 }
